0e791d8e833f15d919c2cc33e83546024639c0ca
Krish <krish@Glebs-MacBook-Pro.local>
2023-05-26 20:14:18 +0400

    Fixed diffe formula, added decode Func - works wrong

--- chiper_decoder/main.go
+++ chiper_decoder/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"math"
+	"os"
 	"strings"
 	"unicode"
 )
@@ -18,27 +20,39 @@ func encodeMessage(offset int, inputString string) string {
 		if position == -1 {
 			encodedString += string(element)
 		} else {
-			if (offset + position) >= len(alfabetString) {
-				fmt.Println(len(alfabetString))
-				fmt.Println(offset + position)
-				newPosition := math.Abs(float64(len(alfabetString) - (offset + position)))
-				fmt.Println(newPosition)
-				if unicode.IsUpper(element) {
-					encodedString += strings.ToUpper(string(alfabetString[int(newPosition)]))
-				} else {
-					encodedString += string(alfabetString[int(newPosition)])
-				}
-			} else if (offset + position) < len(alfabetString) {
-				newPosition := offset + position
-				if unicode.IsUpper(element) {
-					encodedString += strings.ToUpper(string(alfabetString[newPosition]))
-				}
+			if unicode.IsUpper(element) {
+				newPosition := (position + offset) % len(alfabetString)
+				encodedString += strings.ToUpper(string(alfabetString[newPosition]))
+			} else {
+				newPosition := (position + offset) % len(alfabetString)
 				encodedString += string(alfabetString[newPosition])
 			}
 		}
 	}
 	return encodedString
 }
+
+func decodeMessage(offset int, inputString string) string {
+	var decodedString string
+	var position int
+	for _, element := range inputString {
+		position = strings.Index(alfabetString, strings.ToLower(string(element)))
+		if position == -1 {
+			decodedString += string(element)
+		} else {
+			if unicode.IsUpper(element) {
+				newPosition := int(math.Abs(float64((position - offset) % len(alfabetString))))
+				decodedString += strings.ToUpper(string(alfabetString[newPosition]))
+			} else {
+				newPosition := int(math.Abs(float64((position - offset) % len(alfabetString))))
+				decodedString += string(alfabetString[newPosition])
+			}
+		}
+	}
+	return decodedString
+}
+
+// replace all calculation by modulus
 
 func main() {
 	// write your code here
@@ -54,14 +68,14 @@ func main() {
 	var A, B, S int
 	A, B, S = 1, 1, 1
 
-	for i := 1; i <= b; i++ {
+	for i := 0; i < b; i++ {
 		B = B * g % p
 	}
 
 	fmt.Scanf("A is %d", &A)
 
-	for i := 1; i <= A; i++ {
-		S = S * B % p
+	for i := 0; i < b; i++ {
+		S = S * A % p
 	}
 
 	fmt.Printf("B is %d\n", B)
@@ -70,17 +84,21 @@ func main() {
 
 	// index := strings.Index(alfabetString, "e")
 
-	offset := S % len(alfabetString) // create real offset
-
 	/*var inputForEncode string
 	fmt.Print("Please enter string for encode:")
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
 	inputForEncode = scanner.Text()*/
 
-	fmt.Println(S)
-	fmt.Println(offset)
-	fmt.Print(encodeMessage(offset, willYouMerry))
+	offset := S % len(alfabetString)
+
+	fmt.Println(encodeMessage(offset, willYouMerry))
+
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	inputForDecode := scanner.Text()
+
+	fmt.Println(decodeMessage(offset, inputForDecode))
 
 	/*for _, element := range strings.ToLower(inputForEncode) {
 		position := strings.Index(alfabetString, string(element)) // if not exist we receive -1 paste as is
