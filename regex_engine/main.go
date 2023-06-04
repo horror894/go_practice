9fdff8a0b4d87c7ef47f87d77c47bd0510b69d71
Krish <krish@Glebs-MacBook-Pro.local>
2023-06-05 00:15:44 +0400

    Added support for "*" "+" "?", but algoritm to complicated, decomposition will have to be done

--- regex_engine/main.go
+++ regex_engine/main.go
@@ -20,6 +20,41 @@ func regexpMatch(regexp, input string) bool {
 	if input == "" {
 		return false
 	}
+	if len(regexp) >= 2 {
+		if string(regexp[1]) == "?" {
+			if regexp[0] == input[0] || string(regexp[0]) == "." {
+				return regexpMatch(regexp[2:], input[1:])
+			} else {
+				return regexpMatch(regexp[2:], input)
+			}
+		}
+		if string(regexp[1]) == "*" {
+			if regexp[0] == input[0] || string(regexp[0]) == "." {
+				if !regexpMatch(regexp, input[1:]) {
+					if len(regexp[2:]) == len(input[1:]) {
+						return regexpMatch(regexp[2:], input[1:])
+					}
+					return regexpMatch(regexp[2:], input[1:])
+				}
+				return regexpMatch(regexp, input[1:])
+			} else {
+				return regexpMatch(regexp[2:], input)
+			}
+		}
+		if string(regexp[1]) == "+" {
+			if regexp[0] == input[0] || string(regexp[0]) == "." {
+				if !regexpMatch(regexp, input[1:]) {
+					if len(regexp[2:]) == len(input[1:]) {
+						return regexpMatch(regexp[2:], input[1:])
+					}
+					return regexpMatch(regexp[2:], input[1:])
+				}
+				return regexpMatch(regexp, input[1:])
+			} else {
+				return false
+			}
+		}
+	}
 	if regexp[0] == input[0] || string(regexp[0]) == "." {
 		return regexpMatch(regexp[1:], input[1:])
 	}
